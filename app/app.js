ee343a96842ac0994b0abec1b3a8e53250b07f86
peazzoni1 <peazzoni@yahoo.com>
2016-02-10 09:04:03 -0500

    initial commit

--- app/app.js
+++ app/app.js
@@ -13,6 +13,38 @@ App = Ember.Application.extend({
   Resolver
 });
 
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    var that = this;
+    that.localWeather = " ";
+    that.lat = "0";
+    that.lon = "0";
+
+    that.getWeather = function(position) {
+        that.lat = position.coords.latitude;
+        that.lon = position.coords.longitude;
+        Ember.$.getJSON('http://api.openweathermap.org/data/2.5/weather', {
+          lat: that.lat,
+          lon: that.lon,
+          units: 'imperial',
+          APPID: 'c2845ce628770f05c0ea1650fb1e66fe'}).then(function(data) {
+          //that.localWeather = data;
+          that.store.push('weather', that.store.normalize(JSON.stringify(data)));
+          //that.payload.save();
+          console.log(JSON.stringify(data));
+        });
+      }
+    that.getLocation = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(that.getWeather);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+    that.getLocation();
+  }
+  });
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
